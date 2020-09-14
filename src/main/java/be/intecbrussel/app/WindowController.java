2fd957fa4afae7862458b1d0ca3a8ffacc670805
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-14 17:00:36 +0200

    Click Counter

--- src/main/java/be/intecbrussel/app/WindowController.java
+++ src/main/java/be/intecbrussel/app/WindowController.java
@@ -1,7 +1,21 @@
 package be.intecbrussel.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class WindowController {
+    @FXML
+    private Label buttonLabel;
+
+    private int counter = 0;
+
+    @FXML
     public void clickMeButtonClicked(){
-        System.out.println("Testing...");
+        if (!buttonLabel.isVisible()){
+            buttonLabel.setVisible(true);
+        }
+
+        counter++;
+        buttonLabel.setText("Amount of times the button is clicked: " + counter);
     }
 }
