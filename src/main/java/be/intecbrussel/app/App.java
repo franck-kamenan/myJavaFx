c78acd708d699d723987b0bc933bc6d1d80e88c7
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-14 13:52:18 +0200

    Empty Window

---
+++ src/main/java/be/intecbrussel/app/App.java
@@ -0,0 +1,21 @@
+package be.intecbrussel.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("JavaFxDemo");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/window.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
