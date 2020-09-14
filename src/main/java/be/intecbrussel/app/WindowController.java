1e27ae32e7f2c50a64e5fd509d8e9b289ed09fa7
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-14 18:45:32 +0200

    init test

--- src/main/java/be/intecbrussel/app/WindowController.java
+++ src/main/java/be/intecbrussel/app/WindowController.java
@@ -2,20 +2,33 @@ package be.intecbrussel.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 public class WindowController {
     @FXML
-    private Label buttonLabel;
-
-    private int counter = 0;
+    private TextField amount;
 
     @FXML
-    public void clickMeButtonClicked(){
-        if (!buttonLabel.isVisible()){
-            buttonLabel.setVisible(true);
-        }
+    private TextField tipAmount;
 
-        counter++;
-        buttonLabel.setText("Amount of times the button is clicked: " + counter);
+    @FXML
+    private TextField totalAmount;
+
+    @FXML
+    private Slider tipSlider;
+
+    @FXML
+    private Label percentage;
+
+    @FXML
+    private void calculateTip(){
+
+    }
+
+    @FXML
+    public void initialize(){
+        percentage.setText("init test");
+        System.out.println("test");
     }
 }
